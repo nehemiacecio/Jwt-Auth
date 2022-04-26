47947c0d5edaa7445cf3e8caf638c258df0c4b10
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-04-26 23:30:36 +0700

    Change Folder Structure

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -7,7 +7,7 @@ export const refreshToken = async (req, res) => {
         if (!refreshToken) return res.sendStatus(401);
         const user = await Users.findAll({
             where: {
-                refresh_token : refreshToken
+                refresh_token: refreshToken
             }
         });
         if (!user[0]) return res.sendStatus(403)
@@ -16,7 +16,7 @@ export const refreshToken = async (req, res) => {
             const userId = user[0].id;
             const name = user[0].name;
             const email = user[0].email;
-            const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET, {
+            const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '15s'
             })
             res.json({ accessToken })
