dbcb205e1f728c95ae58de12d458b29320d94a4b
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-04-26 23:56:28 +0700

    Berhasil komunikasi sama service billy

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 import express from "express";
+// import {
+//     getUsers, Register, Login, Logout
+// } from "../controllers/Users.js";
 import {
     getUsers, Register, Login, Logout
-} from "../controllers/Users.js";
+} from "../controllers/UsersAPI.js";
+
 import {verifyToken} from "../middleware/VerifyToken.js";
 import {refreshToken} from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/users',verifyToken, getUsers);
+// router.get('/users',verifyToken, getUsers);
+router.get('/users', getUsers);
 router.post('/users',Register);
 router.post('/login',Login);
 router.get('/token',refreshToken);
